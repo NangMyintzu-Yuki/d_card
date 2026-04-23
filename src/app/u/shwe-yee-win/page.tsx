1f713c2bb80f26e3eeb2dfdfa4a4c114b98431ec
Nang Myintzu <nangmyintzu89@gmail.com>
2026-04-23 22:01:18 +0630

    fix

--- src/app/u/shwe-yee-win/page.tsx
+++ src/app/u/shwe-yee-win/page.tsx
@@ -1,30 +1,32 @@
 import Template7 from '@/components/templates/Template7';
 import ProfileImg from '../../../../public/images/shweyeewin.jpg';
-
-const myData = {
-    name: "Shwe Yee Win",
-    title: "Founder Of WeGrow Myanmar",
-    email: "shweyeewin.wegrow@gmail.com",
-    phone: "09974133003",
-    description: "Founder of WeGrow Myanmar...",
-    followUrl: "https://www.facebook.com/share/1DeZXuc5U4/"
-};
 //  const corporateLinks = [
-//         { title: 'Company Website', icon: <Globe size={18} className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1DeZXuc5U4/' },
-//         { title: 'WeGrow', icon: <Calendar size={18} className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1DeZXuc5U4/' },
-//         { title: 'WeTech', icon: <TrendingUp className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1HxKwZSuzV/' },
-//         { title: 'Awareness for Excellence - A4E', icon: <Atom className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1EEqNicLMs/' },
-//     ];
-
-const myLinks = [
+    //         { title: 'Company Website', icon: <Globe size={18} className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1DeZXuc5U4/' },
+    //         { title: 'WeGrow', icon: <Calendar size={18} className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1DeZXuc5U4/' },
+    //         { title: 'WeTech', icon: <TrendingUp className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1HxKwZSuzV/' },
+    //         { title: 'Awareness for Excellence - A4E', icon: <Atom className="text-slate-500 group-hover:text-amber-500" />, url: 'https://www.facebook.com/share/1EEqNicLMs/' },
+    //     ];
     
-     { platform: "Viber", label:'Viber', color:'text-purple-600', url: "viber://add?number=09974133003" },
-        { platform: "Facebook",label:"Facebook", color:"text-blue-600", url: "https://www.facebook.com/share/1KSC1gD3Wd/" },
-        { platform: "Linkedin",label:"Linkedin",color:"text-[#0077B5]", url: "https://www.linkedin.com/in/shwe-yee-win-767aaa160" },
-        { platform: "Telegram", label:"Telegram",color:"bg-[#0088CC] text-[#0088CC]", url: "https://t.me/+959974133003" },
-        { platform: "WhatsApp", label:"WhatsApp",color:"text-green-600", url: "https://wa.me/959671496721" },
-];
+    const myLinks = [
+        
+        { platform: "Viber",  url: "viber://add?number=09974133003" },
+        { platform: "Facebook",url: "https://www.facebook.com/share/1DeZXuc5U4/" },
+        { platform: "Linkedin", url: "https://www.linkedin.com/in/shwe-yee-win-767aaa160" },
+        { platform: "Telegram",url: "https://t.me/+959974133003" },
+        { platform: "WhatsApp", url: "https://wa.me/959671496721" },
+    ];
+    
+    const myData = {
+        name: "Shwe Yee Win",
+        title: "Founder Of WeGrow Myanmar",
+        email: "shweyeewin.wegrow@gmail.com",
+        phone: "09974133003",
+        description: "Founder of WeGrow Myanmar...",
+        profileImage: ProfileImg,
+        socialLinks: myLinks,
+        corporateLinks: [],
+    };
 
 export default function Page() {
-    return <Template7 customer={myData} socialLinks={myLinks} profileImage={ProfileImg} />;
+    return <Template7 data={myData} />;
 }
--- src/app/u/zwe-aung/page.tsx
+++ src/app/u/zwe-aung/page.tsx
@@ -3,9 +3,9 @@ import Profile from '../../../../public/images/zwe_aung.jpg'
 import Template7 from '@/components/templates/Template7';
 
 const myLinks = [
-    { platform: 'Facebook', color: 'text-blue-600', label: 'Facebook', url: "https://www.facebook.com/share/1AkxfNBFKW/" },
-    { platform: 'Viber', color: 'text-indigo-400', label: 'Viber', url: "viber://add?number=09979052919" },
-    { platform: 'Telegram', color: 'text-blue-400', label: 'Telegram', url: "https://t.me/zweaung_nova_edu" },
+    { platform: 'Facebook', url: "https://www.facebook.com/share/1AkxfNBFKW/" },
+    { platform: 'Viber', url: "viber://add?number=09979052919" },
+    { platform: 'Telegram',  url: "https://t.me/zweaung_nova_edu" },
 ];
 
 
@@ -14,12 +14,15 @@ const myData = {
     title: "Founder & CEO of NOVA Education Services",
     email: "zweaung.nova.edu@gmail.com",
     phone: "09794363231",
-    address: "No.404 B, Tayza Street, 35 Ward, North Dagon."
+    address: "No.404 B, Tayza Street, 35 Ward, North Dagon.",
+    corporateLinks: [],
+    profileImage:Profile,
+    socialLinks:myLinks,
 };
 export default function DigitalCard() {
   
     return (
-        <Template7  customer={myData} socialLinks={myLinks} profileImage={Profile} />
+        <Template7  data={myData}  />
     )
 }
 
